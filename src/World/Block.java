e0d89a52f2eff3b3b11161fdbb4fe810bef411bd
i13050 <i13050@LAB03-07.ftis.unpar>
2014-10-24 08:23:34 +0700

    new commit

---
+++ src/World/Block.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package World;
+
+/**
+ *
+ * @author i13050
+ */
+public class Block {
+    
+}
